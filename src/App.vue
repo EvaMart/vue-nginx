535726fbe6a7cd11050f68fa0f86b3c609affa5d
eva <eva@bsclife015.bsc.es>
2022-05-05 14:17:53 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Logo" src="./assets/coming_soon.svg">
+  <HelloWorld msg="Welcome to Vue-Nginx Project"/>
 </template>
 
 <script>
